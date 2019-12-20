2432b26ddefd04ef2bd4599c786363ad0c4130cd
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-12-20 19:57:13 +0400

    **Maybe it works,who knows**

--- src/main/java/app/Controller/PizzaController.java
+++
@@ -1,9 +0,0 @@
-package app.Controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PizzaController {
-
-}
