cbaeebced7e71badbfd244dffd58933a75c1549d
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-12-20 19:58:45 +0400

    **Maybe it works,who knows**

---
+++ src/main/java/app/Controller/MainPageController.java
@@ -0,0 +1,7 @@
+package app.Controller;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainPageController {
+
+}
---
+++ src/main/java/app/Controller/OrderController.java
@@ -0,0 +1,8 @@
+package app.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+}
